f0329c0f027190432151ea410dec9703e4c4363a
Bikram Chettri <bikramchettri120@outlook.com>
2024-12-13 20:16:04 +0530

    template engine completed and function is autoloded

---
+++ src/App/Controllers/AboutController.php
@@ -0,0 +1,28 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Controllers;
+
+use App\Config\Paths;
+use Framework\TemplateEngine;
+
+class AboutController
+{
+    private TemplateEngine $view;
+
+    public function __construct()
+    {
+        $this->view = new TemplateEngine(Paths::VIEW);
+    }
+
+    public function home()
+    {
+        echo $this->view->render('about.php', [
+            'title' => 'About',
+            'dangerousData' => '<script>alert(123)</script>'
+        ]);
+        // dd($this->view);
+        // echo 'home page';
+    }
+}
--- src/App/bootstrap.php
+++ src/App/bootstrap.php
@@ -5,14 +5,18 @@ declare(strict_types=1);
 
 require __DIR__ . '/../../vendor/autoload.php';
 
-
 use Framework\App;
-use App\Controllers\HomeController;
+
+use function App\Config\registerRoutes; //importing functions using use function
+
+// use App\Controllers\{HomeController, AboutController};
 
 $app = new App();
+registerRoutes($app);
 
 // $app->get('/', ['App\Controllers\HomeController', 'home']);
-$app->get('/', [HomeController::class, 'home']);
+// $app->get('/', [HomeController::class, 'home']);
+// $app->get('/about', [AboutController::class, 'home']);
 
 // $app->get('/home');
 // $app->get('/ching/');
