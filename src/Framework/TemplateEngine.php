3597353b328336b51f4b4d5924016f29e3881ee5
Bikram Chettri <bikramchettri120@outlook.com>
2024-12-13 19:28:50 +0530

    Template Engine Output Buffer Added

--- src/Framework/TemplateEngine.php
+++ src/Framework/TemplateEngine.php
@@ -4,4 +4,22 @@ declare(strict_types=1);
 
 namespace Framework;
 
-class TemplateEngine {}
+class TemplateEngine
+{
+    public function __construct(private string $basePath) {}
+
+    public function render(string $template, array $data = [])
+    {
+        extract($data, EXTR_SKIP);
+
+        ob_start();
+
+        include "{$this->basePath}/{$template}";
+
+        $output = ob_get_contents();
+
+        ob_end_clean();
+
+        return $output;
+    }
+}
